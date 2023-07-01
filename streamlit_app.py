331cf2b53c97ac1cfe85989fe9915080a026492d
dhycchan <46333541+dhycchan@users.noreply.github.com>
2023-07-01 17:26:44 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,11 +42,11 @@ my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 
 # Authentication Snowflake
-#my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-#streamlit.text("Hello from Snowflake:")
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 
 # Load table fruit
-my_cur.execute("SELECT * FROM fruit_load_list")
+#my_cur.execute("SELECT * FROM fruit_load_list")
 my_data_row = my_cur.fetchone()
-streamlit.text("The fruit load list contains:")
+streamlit.text("Hello from Snowflake:")
+#streamlit.text("The fruit load list contains:")
 streamlit.text(my_data_row)
